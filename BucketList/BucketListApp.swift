097dcc55992e41129e59985588cab0afe91a26e6
ZachMommaerts <zmommaerts@gmail.com>
2023-11-16 10:49:33 -0600

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Zach Mommaerts on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
